c07480830bcb3c84e52d3c40cc880b1221819538
hale <olcalzeynephale@gmail.com>
2020-07-11 11:00:12 -0400

    usename

--- src/Project.java
+++ src/Project.java
@@ -1,7 +1,13 @@
 import Utility.BaseClass;
+import org.openqa.selenium.By;
+
+import java.util.concurrent.TimeUnit;
 
 public class Project extends BaseClass {
     public static void main(String[] args) {
         driver.get("https://app.hubspot.com/login");
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.findElement(By.id("username")).sendKeys("olcalzeynephale@gmail.com");
+        driver.findElement(By.id("password")).sendKeys("Istanbul1234");
     }
 }
